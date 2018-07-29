32f76997b97b08abd1ac954f29aee0663a69f34b
Lou Foster <lfoster@Lous-Air.zyxel.com>
2018-07-29 12:23:16 -0400

    latticewords web game & site initial commit

---
+++ public/javascripts/latticewords/grid.js
@@ -0,0 +1,462 @@
+const ANIMATE_SPEED = 250;
+const LETTER_COLOR = '#0e267f';
+const SELECT_COLOR = '#2f95ff';
+const BONUS_COLOR = '#dd5';
+const BONUS_STROKE = '#139';
+
+class Grid  {
+   constructor(scene, rows, cols, tileSize, pool) {
+      this.scene = scene;
+      this.rows = rows;
+      this.cols = cols;
+      this.tileSize = tileSize;
+      this.red = false;
+      this.animating = false;
+      this.priorTile = null;
+      this.pool = pool;
+
+      this.bkg = this.scene.add.image(0,0,  'grid-bkg');
+      this.bkg.setOrigin(0,0);
+      this.width =  this.bkg.width;
+      this.height = this.bkg.height;
+      this.tilesGroup = this.scene.add.container(70,40+70, [this.bkg]);
+
+      var grid = this.scene.add.sprite(0,40, 'grid');
+      grid.setOrigin(0,0);
+      this.grid = [ ];
+   }
+
+   empty(  ) {
+      for (var r=0; r<this.rows; r++) {
+         for (var c=0; c<this.cols; c++) {
+            this.grid[r][c].letter.destroy();
+         }
+      }
+      this.red = false;
+      this.animating = false;
+      this.grid = [ ];
+   }
+
+   fill( ) {
+      this.red = false;
+      this.animating = false;
+      this.grid = [ ];
+      for (var r=0; r<this.rows; r++) {
+         this.grid.push( [] );
+         for (var c=0; c<this.cols; c++) {
+            var letter = this.pool.pop();
+            this.grid[r].push( this.addTile(letter, r, c, false, false) );
+         }
+      }
+   }
+
+   destroySelectedTiles(pool) {
+      var coords = [];
+      var letters = [];
+      for ( var r=0; r<this.rows; r++ ) {
+         for ( var c=0; c<this.cols; c++ ) {
+            var tile = this.grid[r][c];
+            if ( tile.selected == true ) {
+               coords.push( r+","+c );
+               letters.push(tile.letter);
+            }
+         }
+      }
+      this.scene.tweens.add({targets: letters, alpha:0,
+         duration: 250, ease: 'Linear',
+         onCompleteParams: [ this ],
+         onComplete: function (tween, targets, self) {
+            for (var j=0; j<coords.length;j++) {
+               var loc = coords[j].split(",");
+               var letterObj = letters[j];
+               letterObj.destroy();
+               var letter = pool.pop();
+               if ( pool.length == 0) {
+                  pool.refill();
+               }
+               self.grid[loc[0]][loc[1]] = self.addTile(letter, loc[0], loc[1] );
+            }
+         }}
+      );
+   }
+
+   checkWords( scoreHandler ) {
+      var words = [];
+      var word = "";
+      var coords = [];
+      var r,c;
+
+      // first find words in rows
+      for (r=0; r<this.rows; r++ ) {
+         for ( c=0;c<this.cols; c++) {
+            if ( this.grid[r][c].selected === true ) {
+               word +=  this.grid[r][c].letter.text;
+               coords.push( r+","+c );
+            } else {
+               if (word.length > 1 ) {
+                  words.push(word);
+               }
+               word = "";
+            }
+         }
+         if (word.length > 1 ) {
+            words.push(word);
+            word = "";
+         }
+      }
+
+      // Now in columns...
+      word = "";
+      for ( c=0; c<this.cols; c++ ) {
+         for ( r=0;r<this.rows; r++) {
+            if ( this.grid[r][c].selected === true ) {
+               word +=  this.grid[r][c].letter.text;
+               var coord = r+","+c;
+               if (coords.includes(coord) == false )  {
+                  coords.push(coord);
+               }
+            } else {
+               if (word.length > 1 ) {
+                  words.push(word);
+               }
+               word = "";
+            }
+         }
+         if (word.length > 1 ) {
+            words.push(word);
+            word = "";
+         }
+      }
+
+      // If no words, bail
+      if (words.length === 0 ) {
+         return;
+      }
+
+      var self = this;
+      axios.post('/check', words.join(","))
+      .then(function (response) {
+         self.handleResults(response.data, coords, scoreHandler );
+      })
+      .catch(function (error) {
+         console.log(error);
+      });
+   }
+
+   handleResults(total, coords, scoreHandler) {
+
+      // If total > 0, all letters score and are replaced
+      // otherwise, the whole submission fails
+      if (total === 0) {
+         this.showFailure(coords);
+         return;
+      }
+
+      var addTime = 0;
+      var tiles = [];
+      for (var j=0; j<coords.length;j++) {
+         var loc = coords[j].split(",");
+         var tile = this.grid[loc[0]][loc[1]];
+         if (tile.bonus == true) {
+            addTime = 20;
+         }
+         tiles.push(tile.letter);
+      }
+
+      scoreHandler(total, addTime);
+
+      // fade out then replace onComplete
+      var self = this;
+      this.scene.tweens.add({targets: tiles ,alpha:0,
+         duration: 250, ease: 'Linear',
+         onComplete: function() {
+            self.replaceUsedTiles(coords);
+         }
+      });
+
+   }
+
+   replaceUsedTiles(coords) {
+      for (var j=0; j<coords.length;j++) {
+         var loc = coords[j].split(",");
+         var tile = this.grid[loc[0]][loc[1]];
+         tile.letter.destroy();
+         var letter = this.pool.pop();
+         if ( this.pool.length == 0) {
+            this.refillLetters();
+         }
+         this.grid[loc[0]][loc[1]] = this.addTile(letter, loc[0], loc[1] );
+      }
+   }
+
+   showFailure(coords) {
+      var tiles = [];
+      for (var j=0; j<coords.length;j++) {
+         var loc = coords[j].split(",");
+         var tile = this.grid[loc[0]][loc[1]];
+         tile.selected = false;
+         tile.letter.setFill("#ff0000");
+         tiles.push(tile);
+      }
+      var d=0;
+      var tint = false;
+      this.scene.tweens.addCounter({
+         from: 0,
+         to: 50,
+         duration: 1000,
+         onUpdate: function () {
+            d+=1;
+            if (d==10) {
+               d = 0;
+               tint = !tint;
+               for ( var i=0; i<tiles.length; i++) {
+                  if (tint==false) {
+                     tiles[i].letter.setFill("#ff0000");
+                  } else {
+                     tiles[i].letter.setFill(LETTER_COLOR);
+                  }
+               }
+            }
+         },
+         onComplete: function() {
+            for ( var i=0; i<tiles.length; i++) {
+               tiles[i].letter.setFill(LETTER_COLOR);
+            }
+         }
+      });
+   }
+
+   flash() {
+      this.red = !this.red;
+      if ( this.red == false) {
+         this.bkg.clearTint();
+      } else {
+         this.bkg.setTint('0xccffff');
+      }
+   }
+
+   addTile(letter, row, col, selected, clone) {
+      var tileX = 42+this.tileSize*col;
+      var tileY = 43+this.tileSize*row;
+      var bonus = false;
+
+      var cfg = {
+         fontSize: '52px',
+         fontWeight: 'bold',
+         fill: LETTER_COLOR,
+         align: 'center'
+      };
+
+      var text = this.scene.add.text( tileX, tileY, letter, cfg);
+      text.setOrigin(0.5);
+      if ( selected === true ) {
+         text.setFill(SELECT_COLOR);
+      }
+      if (clone === false ) {
+         text.alpha = 0;
+         this.scene.tweens.add({targets: text,alpha:1,
+            duration: 250, ease: 'Linear'});
+         if ( this.hasBonus() == false ) {
+            var chance = Math.floor(Math.random() * 100);
+            if ( chance > 96 ) {
+               text.setFill(BONUS_COLOR);
+               text.setStroke(BONUS_STROKE,4);
+               bonus = true;
+            }
+         }
+      }
+
+      this.tilesGroup.add(text);
+      return {letter: text, selected: selected, bonus: bonus};
+   }
+
+   hasBonus() {
+      var cnt = 0;
+      for (var r=0; r<this.rows; r++) {
+         for (var c=0; c<this.cols; c++) {
+            var letter = this.grid[r][c];
+            if (typeof letter === "undefined") return false;
+            if (letter.bonus == true ) {
+               cnt++;
+               if ( cnt === 3) return true;
+            }
+         }
+      }
+      return false;
+   }
+
+   handlePointerUp() {
+      this.priorTile = null;
+   }
+
+   handlePointerDown(screenX, screenY) {
+      var gridX = screenX - 70;
+      var gridY = screenY - 110;
+      console.log("W: "+this.width);
+      if ( gridX <10 || gridY <10 || gridX >= this.width-20 || gridY >= this.height-20) {
+         this.priorTile = null;
+         return;
+      }
+
+      var col = Math.floor( gridX / this.tileSize);
+      var row = Math.floor( gridY / this.tileSize);
+      console.log("X: "+gridX+", Y: "+gridY+" : R="+row+" C="+col);
+      var tile = this.grid[row][col];
+      if ( this.priorTile != tile ) {
+         this.priorTile = tile;
+         if (tile.selected == false ) {
+            tile.selected = true;
+            tile.letter.setFill(SELECT_COLOR);
+         } else {
+            tile.selected = false;
+            if ( tile.bonus ) {
+               tile.letter.setFill(BONUS_COLOR);
+            } else {
+               tile.letter.setFill(LETTER_COLOR);
+            }
+         }
+      }
+   }
+
+   shiftTiles(activeButton) {
+      switch (activeButton[0]) {
+         case "R":
+            this.shiftRight( parseInt(activeButton[1],10) );
+            break;
+         case "L":
+            this.shiftLeft( parseInt(activeButton[1],10) );
+            break;
+         case "U":
+            this.shiftUp( parseInt(activeButton[1],10) );
+            break;
+         case "D":
+            this.shiftDown( parseInt(activeButton[1],10) );
+            break;
+      }
+   }
+
+   shiftRight(row) {
+      if (this.animating) return;
+
+      this.animating = true;
+      var tiles = this.grid[row];
+      var lastTile = tiles[tiles.length-1];
+      var standIn = this.addTile( lastTile.letter.text,row,-1, lastTile.selected, true);
+      var targets = [standIn.letter];
+      for (var i=0; i<this.cols; i++) {
+         targets.push(tiles[i].letter);
+      }
+      this.scene.tweens.add({
+         targets: targets,
+         x: '+='+this.tileSize,
+         duration: ANIMATE_SPEED,
+         ease: 'Linear',
+         onCompleteParams: [ this ],
+         onComplete: function (tween, targets, self) {
+            for (var j=tiles.length-1; j>0; j-- ) {
+               var tile = tiles[j-1];
+               tiles[j] = tile;
+            }
+            tiles[0] = standIn;
+            lastTile.letter.destroy();
+            self.animating = false;
+         }
+      });
+   }
+
+   shiftLeft(row) {
+      if (this.animating) return;
+
+      this.animating = true;
+      var tiles = this.grid[row];
+      var firstTile = tiles[0];
+      var standIn = this.addTile( firstTile.letter.text,row, this.rows, firstTile.selected, true);
+      var targets = [standIn.letter];
+      for (var i=0; i<this.cols; i++) {
+         targets.push(tiles[i].letter);
+      }
+      var tween = this.scene.tweens.add({
+         targets: targets,
+         x: '-='+this.tileSize,
+         duration: ANIMATE_SPEED,
+         ease: 'Linear',
+         onCompleteParams: [ this ],
+         onComplete: function (tween, targets, self) {
+
+            for (var j=0; j<tiles.length-1; j++ ) {
+               var tile = tiles[j+1];
+               tiles[j] = tile;
+            }
+            tiles[tiles.length-1] = standIn;
+            firstTile.letter.destroy();
+            self.animating = false;
+         }
+      });
+   }
+
+   shiftUp(col) {
+      if (this.animating) return;
+
+      this.animating = true;
+      var tiles = [];
+      var targets = [];
+      for (var r=0; r<this.rows; r++) {
+         var tileInfo = this.grid[r][col];
+         tiles.push( tileInfo );
+         targets.push( tileInfo.letter);
+      }
+      var firstTile = tiles[0];
+      var standIn = this.addTile( firstTile.letter.text, this.rows, col, firstTile.selected, true);
+      targets.push( standIn.letter);
+
+      var tween = this.scene.tweens.add({
+         targets: targets,
+         y: '-='+this.tileSize,
+         duration: ANIMATE_SPEED,
+         ease: 'Linear',
+         onCompleteParams: [ this ],
+         onComplete: function (tween, targets, self) {
+            for (var j=0; j<tiles.length-1; j++ ) {
+               var tile = tiles[j+1];
+               self.grid[j][col] = tile;
+            }
+            self.grid[tiles.length-1][col] = standIn;
+            firstTile.letter.destroy();
+            self.animating = false;
+         }
+      });
+   }
+
+   shiftDown(col) {
+      if (this.animating) return;
+
+      this.animating = true;
+      var tiles = [];
+      var targets = [];
+      for (var r=0; r<this.rows; r++) {
+         var tileInfo = this.grid[r][col];
+         tiles.push( tileInfo );
+         targets.push( tileInfo.letter);
+      }
+      var lastTile = tiles[tiles.length-1];
+      var standIn = this.addTile( lastTile.letter.text, -1, col, lastTile.selected, true);
+      targets.push( standIn.letter);
+
+      var tween = this.scene.tweens.add({
+         targets: targets,
+         y: '+='+this.tileSize,
+         duration: ANIMATE_SPEED,
+         ease: 'Linear',
+         onCompleteParams: [ this ],
+         onComplete: function (tween, targets, self) {
+            for (var j=tiles.length-1; j>0; j-- ) {
+               var tile = tiles[j-1];
+               self.grid[j][col] = tile;
+            }
+            self.grid[0][col] = standIn;
+            lastTile.letter.destroy();
+            self.animating = false;
+         }
+      });
+   }
+}
---
+++ public/javascripts/latticewords/latticewords.js
@@ -0,0 +1,321 @@
+// Game constants ===================
+const TILE_SIZE= 75;
+const GRID_X = 77+35;
+const GRID_Y = 77+35+40;
+const BOARD_X = 60+19;
+const BOARD_Y = 120;
+const ROWS = 6;
+const COLS = 6;
+const START_TIME = 180;
+
+class Latticewords extends Phaser.Scene {
+   constructor ()   {
+      super({ key: 'latticewords' });
+   }
+   preload () {
+      this.load.image('grid-bkg', 'images/latticewords/grid-bkg.png');
+      this.load.spritesheet('pause', 'images/latticewords/pause.png',  { frameWidth: 32, frameHeight: 32 });
+      this.load.image('grid', 'images/latticewords/grid.png');
+      this.load.image('pause-bkg', 'images/latticewords/pause=bkg.png');
+      this.load.image('left', 'images/latticewords/left.png');
+      this.load.image('right', 'images/latticewords/right.png');
+      this.load.image('up', 'images/latticewords/up.png');
+      this.load.image('down', 'images/latticewords/down.png');
+      this.load.image('clear', 'images/latticewords/clear.png');
+      this.load.image('check', 'images/latticewords/check.png');
+   }
+
+   create () {
+      this.pool = new Pool();
+      this.pool.refill();
+      this.animating = false;
+      this.activeButton = "";
+      this.clearsRemaining = 3;
+      this.gameOver = false;
+      this.pointerDown = false;
+      this.paused = false;
+      this.clearsRemaining = 3;
+
+      this.textCfg = {
+         fontFamily: 'Arial',
+         fill: '#fff',
+         stroke: "#222",
+         strokeThickness: 6
+      };
+
+      this.addScoreAndTimer();
+
+      this.grid = new Grid(this, ROWS, COLS, TILE_SIZE, this.pool);
+      this.grid.fill();
+
+      this.addRowControls();
+      this.addColControls();
+      this.createPauseMenu();
+      this.createGameOverMenu();
+
+      this.clearBtn = this.add.sprite(0,640, 'clear');
+      this.clearBtn.setInteractive();
+      this.clearBtn.setOrigin(0,1);
+      this.clearBtn.on('pointerdown', this.onClearClick, this);
+      this.clearBtn.on('pointerover', function() {
+         if (this.clearsRemaining > 0) {
+            this.clearBtn.setTint('0xcc6666');
+         }
+      },this);
+      this.clearCounter = this.add.text( 24, 620, "3", this.textCfg);
+      this.clearCounter.setOrigin(0.5,0.5);
+      this.clearCounter.setFontSize(28);
+      this.clearCounter.setStroke(0,4);
+
+      var check = this.add.image(600,640, 'check');
+      check.setOrigin(1,1);
+      check.setInteractive();
+      check.on('pointerdown', this.onCheckClick, this);
+      check.on('pointerover', function() {
+         check.setTint('0x00cc00');
+      },this);
+
+
+      // all game objects highlight yellow when moused over
+      this.input.on('gameobjectover', function (pointer, gameObject) {
+         if (gameObject == this.clearBtn) return;
+         gameObject.setTint('0x00ffff');
+      }, this);
+      this.input.on('gameobjectout', function (pointer, gameObject) {
+         gameObject.clearTint();
+      });
+
+      // Mouse handlers
+      this.input.on('pointerdown', function (pointer) {
+         if (this.paused || this.gameOver ) return;
+         this.pointerDown = true;
+         this.grid.handlePointerDown(pointer.x, pointer.y);
+      }, this);
+
+      this.input.on('pointerup', function () {
+         console.log("GRID UP");
+         this.pointerDown = false;
+         if (this.paused || this.gameOver ) return;
+         this.grid.handlePointerUp();
+      }, this);
+
+       this.input.on('pointermove', function (pointer) {
+          if (this.paused || this.gameOver ) return;
+          if ( this.pointerDown ) {
+             this.grid.handlePointerDown(pointer.x, pointer.y);
+           }
+       }, this);
+   }
+
+   update() {
+      if (this.gameOver == true || this.paused == true) return;
+      if ( this.activeButton != "" ) {
+         this.grid.shiftTiles(this.activeButton);
+         return;
+      }
+   }
+
+   restartGame() {
+      this.score = 0;
+      this.gameOver = false;
+      this.clearsRemaining = 3;
+      this.clearCounter.setText("3");
+      this.timeLeft = START_TIME;
+      var minutes = Math.floor(this.timeLeft / 60);
+      var seconds = (this.timeLeft - minutes * 60);
+      this.timerDisplay.setText(  (("0"+minutes).substr(-2) + ":" + ("0"+seconds).substr(-2)));
+      this.animating = false;
+      this.activeButton = "";
+      this.pool.refill();
+      this.grid.empty();
+      this.grid.fill();
+      this.pauseGroup.visible = false;
+      this.gameOverGroup.visible = false;
+      this.paused = false;
+      this.clearBtn.alpha = 1;
+      this.clearCounter.alpha = 1;
+      this.pauseBtn.setFrame(0);
+      this.gameTimer.paused = false;
+   }
+
+   createGameOverMenu() {
+      var text = this.add.text( 230, 80, "Game Over", this.textCfg);
+      text.setFontSize(36);
+      text.setOrigin(0.5);
+      var pauseBkg = this.add.image(0,0,  'pause-bkg');
+      pauseBkg.setOrigin(0,0);
+
+      var restart = this.add.text( 230, 220, "Play Again?", this.textCfg);
+      restart.setFontSize(24);
+      restart.setOrigin(0.5);
+      restart.setInteractive();
+      restart.on('pointerup', function(pointer,x,y) {
+         this.restartGame();
+      }, this);
+
+      this.bestLabel = this.add.text( 230, 350, "Best Score", this.textCfg);
+      this.bestLabel.setFontSize(22);
+      this.bestLabel.setOrigin(0.5);
+      this.bestScore = this.add.text( 230, 380, "0", this.textCfg);
+      this.bestScore.setFontSize(16);
+      this.bestScore.setOrigin(0.5);
+      var bestScore = Cookies.get('bestScore');
+      if ( bestScore ) {
+         this.bestScore.setText(bestScore);
+      }
+
+      this.gameOverGroup = this.add.container((600-460)/2, 110, [
+         pauseBkg, text, restart, this.bestLabel, this.bestScore]);
+      this.gameOverGroup.setVisible(false);
+   }
+
+   createPauseMenu() {
+      var text = this.add.text( 230, 80, "Game Paused", this.textCfg);
+      text.setFontSize(36);
+      text.setOrigin(0.5);
+      var pauseBkg = this.add.image(0,0,  'pause-bkg');
+      pauseBkg.setOrigin(0,0);
+
+      var resume = this.add.text( 230, 200, "Resume", this.textCfg);
+      resume.setFontSize(24);
+      resume.setOrigin(0.5);
+      resume.setInteractive();
+      resume.on('pointerup', function(pointer,x,y) {
+         this.pauseBtn.setFrame(0);
+         this.gameTimer.paused = false;
+         this.pauseGroup.setVisible(false);
+         this.paused = false;
+      }, this);
+
+      var restart = this.add.text( 230, 270, "Restart", this.textCfg);
+      restart.setFontSize(24);
+      restart.setOrigin(0.5);
+      restart.setInteractive();
+      restart.on('pointerup', function(pointer,x,y) {
+         this.restartGame();
+      }, this);
+
+      this.pauseGroup = this.add.container((600-460)/2, 110, [ pauseBkg, text, resume, restart]);
+      this.pauseGroup.setVisible(false);
+   }
+
+   addScoreAndTimer() {
+      this.score = 0;
+      this.timeLeft = START_TIME;
+
+      var rect = new Phaser.Geom.Rectangle(0, 0, 600, 40);
+      var gfx = this.add.graphics({ fillStyle: { color: 0x00000099 } });
+      gfx.fillRectShape(rect);
+
+      this.scoreDisplay = this.add.text( 10,3, "000000", this.textCfg);
+      this.scoreDisplay.setFontSize(26);
+      this.timerDisplay =  this.add.text( 590,3, "03:00", this.textCfg);
+      this.timerDisplay.setFontSize(26);
+      this.timerDisplay.setOrigin(1,0);
+      this.gameTimer = this.time.addEvent({ delay: 1000, callback: this.tick, callbackScope: this, loop: true });
+
+      // pause / play button
+      this.paused = false;
+      this.pauseBtn = this.add.sprite(300,4, 'pause');
+      this.pauseBtn.setInteractive();
+      this.pauseBtn.setOrigin(0.5,0);
+      this.pauseBtn.on('pointerdown', function() {
+         this.paused  = !this.paused;
+          if ( this.paused == true ) {
+              this.pauseBtn.setFrame(1);
+              this.gameTimer.paused = true;
+              this.pauseGroup.setVisible(true);
+          } else {
+             this.pauseBtn.setFrame(0);
+             this.gameTimer.paused = false;
+             this.pauseGroup.setVisible(false);
+          }
+
+      }, this);
+   }
+
+   addShiftButton(x,y, id, image ) {
+      var name = image[0].toUpperCase()+id;
+      var btn = this.add.sprite(x,y, image);
+      btn.on('pointerdown', function() {
+         this.activeButton = btn.name;
+      }, this);
+      btn.on('pointerup', function() {
+         this.activeButton = "";
+      }, this);
+      btn.setName(name);
+      btn.setOrigin(0.5, 0.5);
+      btn.setInteractive();
+   }
+
+   addRowControls() {
+      var ds = TILE_SIZE;
+      for ( var r=0; r < ROWS; r++) {
+         this.addShiftButton(54, GRID_Y+ds*r, r, 'left');
+         this.addShiftButton(600-55, GRID_Y+ds*r, r, 'right');
+      }
+   }
+
+   addColControls() {
+      var ds = TILE_SIZE;
+      for ( var c=0; c<COLS; c++) {
+         this.addShiftButton(GRID_X+ds*c,94, c, 'up');
+         this.addShiftButton(GRID_X+ds*c,640-55, c, 'down');
+      }
+   }
+
+   tick() {
+      if (this.gameOver) return;
+
+      this.timeLeft--;
+      var minutes = Math.floor(this.timeLeft / 60);
+      var seconds = (this.timeLeft - minutes * 60);
+      this.timerDisplay.setText(  (("0"+minutes).substr(-2) + ":" + ("0"+seconds).substr(-2)));
+      var scoreTxt = ""+this.score;
+      this.scoreDisplay.setText( scoreTxt.padStart(6, "0"));
+
+      if ( this.timeLeft <= 15 ) {
+         this.grid.flash();
+      }
+      if (this.timeLeft == 0) {
+         this.gameOver = true;
+         this.gameOverGroup.setVisible(true);
+         var bestScore = parseInt(Cookies.get('bestScore'),10);
+         if ( !bestScore ) bestScore = 0;
+         if ( this.score > bestScore ) {
+            Cookies.set('bestScore', this.score);
+            this.bestScore.setText(this.score);
+            this.bestLabel.setText("New Best Score!");
+         } else {
+            this.bestLabel.setText("Best Score");
+         }
+      }
+   }
+
+   /**
+   * Button Handlers
+   */
+   onClearClick() {
+      if (this.paused || this.gameOver || this.clearsRemaining  == 0) return;
+
+      this.clearsRemaining -= 1;
+      this.clearCounter.setText( ""+this.clearsRemaining);
+      if ( this.clearsRemaining == 0) {
+         this.clearBtn.alpha = 0.5;
+         this.clearCounter.alpha = 0.5;
+      }
+      this.grid.destroySelectedTiles(this.pool);
+   }
+   onCheckClick() {
+      if (this.paused || this.gameOver) return;
+      var self = this;
+      this.grid.checkWords( function(points, addedTime) {
+         self.scoreHandler(points, addedTime);
+      } );
+   }
+
+   scoreHandler(points, addedTime) {
+      this.score += points;
+      this.timeLeft += addedTime;
+   }
+}
