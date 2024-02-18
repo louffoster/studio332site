95a3179dba2732d398d554f9d3cc979beb335eb7
Lou Foster <loufoster@gmail.com>
2024-02-18 16:50:39 -0500

    test layout of wordmine screen

--- frontend/src/games/wordmine/game.js
+++ frontend/src/games/wordmine/game.js
@@ -1,12 +1,54 @@
 import BasePhysicsGame from "@/games/common/basephysicsgame"
+import Rock from "@/games/wordmine/rock"
+import LetterPool from "@/games/common/letterpool"
+import Matter from 'matter-js'
 
 export default class WordMine extends BasePhysicsGame {
+   pool = new LetterPool()
 
    initialize(replayHandler, backHandler) {
-      
+      // set bottom aand sides
+      let b = Matter.Bodies.rectangle(this.gameWidth/2, this.gameHeight+25, this.gameWidth, 50, { isStatic: true, friction: 0, restitution: 0})
+      Matter.Composite.add(this.physics.world, b)
+      let l = Matter.Bodies.rectangle(-25, this.gameHeight/2, 50, this.gameHeight, { isStatic: true, friction: 0, restitution: 0})
+      Matter.Composite.add(this.physics.world, l)
+      let r = Matter.Bodies.rectangle(this.gameWidth+25, this.gameHeight/2, 50, this.gameHeight, { isStatic: true, friction: 0, restitution: 0})
+      Matter.Composite.add(this.physics.world, r)
+
+      // fill in rocks
+      this.pool.refill()
+      let y = this.gameHeight - Rock.HEIGHT/2
+      let x = Rock.WIDTH/2
+      for ( let c=0; c< 6; c++) {
+         for ( let r=0; r< 10; r++) {
+            let ltr = this.pool.popScoringLetter()
+            console.log(ltr)
+            let rock = new Rock( x,y, ltr )
+            this.addPhysicsItem( rock )
+            y -= Rock.HEIGHT
+         }
+         y = this.gameHeight - Rock.HEIGHT/2
+         x+= Rock.WIDTH
+      }
+
+      this.draw()
    }
 
    draw() {
+      this.gfx.clear() 
+      this.gfx.beginFill(0x86BBD8)
+      this.gfx.lineStyle(1, 0x86BBD8, 1)
+      this.gfx.drawRect(0,0, this.gameWidth, 105)
+      this.gfx.beginFill(0x73a942)
+
+      this.gfx.lineStyle(3, 0x656D4A, 1)
+      this.gfx.drawRect(0,105, this.gameWidth, 20)
+
+      this.gfx.beginFill(0xA68A64)
+      this.gfx.lineStyle(2, 0x582F0E, 1)
+      this.gfx.drawRect(330,125, this.gameWidth-330, this.gameHeight-125)
+
+      this.gfx.endFill()
    }
 
    update() {
---
+++ frontend/src/games/wordmine/rock.js
@@ -0,0 +1,102 @@
+import BasePhysicsItem from "@/games/common/basephysicsitem"
+import * as PIXI from "pixi.js"
+import Matter from 'matter-js'
+
+export default class Rock extends BasePhysicsItem {
+   letter = null 
+   extra = null
+   static WIDTH = 55
+   static HEIGHT = 55
+
+   constructor( x,y, letter) {
+      super(x,y)
+      console.log(letter)
+
+      this.letter = letter
+      
+      let letterColor = "#efefef"
+      this.lineColor = new PIXI.Color( 0x582F0E )
+      if ( letter.isVowel()) {
+         this.fillColor = new PIXI.Color( 0x6F3F14 )
+      } else  {
+         this.fillColor = new PIXI.Color( 0x7F4F24 )
+      }
+
+      let style = new PIXI.TextStyle({
+         fill: letterColor,
+         fontFamily: "Arial",
+         fontSize: 18,
+      })
+      let smallStyle = new PIXI.TextStyle({
+         fill: letterColor,
+         fontFamily: "Arial",
+         fontSize: 15,
+      })
+
+      let txt = new PIXI.Text(letter.text, style)
+      txt.anchor.set(0.5)
+      txt.x = 0
+      txt.y = 0
+      this.addChild(txt)
+      if (letter.text == "Q") {
+         let uTxt = new PIXI.Text("U", smallStyle)
+         uTxt.anchor.set(0.5)
+         uTxt.x = 8
+         uTxt.y = 4    
+         txt.x-= 4
+         this.addChild(uTxt)
+      }
+
+      let polyPts = [ 
+         {x: -Rock.WIDTH/2, y: -Rock.HEIGHT/4}, 
+         {x: -Rock.WIDTH/4, y: -Rock.HEIGHT/2},
+         {x: +Rock.WIDTH/4, y: -Rock.HEIGHT/2},
+         {x: +Rock.WIDTH/2, y: -Rock.HEIGHT/4},
+         {x: +Rock.WIDTH/2, y: +Rock.HEIGHT/4}, 
+         {x: +Rock.WIDTH/4, y: +Rock.HEIGHT/2},
+         {x: -Rock.WIDTH/4, y: +Rock.HEIGHT/2},
+         {x: -Rock.WIDTH/2, y: +Rock.HEIGHT/4},
+      ]
+
+      // let v = Matter.Vertices.fromPath(`0,0, ${this.w},0, 0,${this.h}`)
+      let c = Matter.Vertices.centre(polyPts) 
+      this.pivot.set(c.x, c.y)  
+      this.body = Matter.Bodies.fromVertices(x, y, polyPts, {isStatic: this.isStatic})
+      // Matter.Body.setCentre(this.body, {x:0,y:0}, true)
+      this.body.label = "rock-"+letter.text
+      this.setAirFriction(0.02)
+      this.setRestitution( 0 )
+
+      this.polygon = new PIXI.Polygon(polyPts)
+
+
+      this.update()
+      this.draw()
+   }
+
+   remove() {
+      this.body.isSensor = true
+      this.gfx.alpha = 0
+   }
+
+   draw() {
+      super.draw() 
+      this.gfx.clear()
+      this.gfx.lineStyle(1, this.lineColor, 1)
+      this.gfx.beginFill(this.fillColor)
+      this.gfx.drawPolygon( this.polygon )
+      this.gfx.endFill()
+   }
+
+   get text() {
+      let out = this.letter.text 
+      if ( out == "Q") {
+         out += "U"
+      }
+      return out
+   }
+
+   get value() {
+      return this.letter.value
+   }
+}
